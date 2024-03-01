faa910fa34004f8061cad3a1f35a60140ae4a284
Eva Tavares <eva.programadora@gmail.com>
2024-03-01 15:57:30 -0300

    add: teste unitário

--- src/characters/characters.controller.spec.ts
+++ src/characters/characters.controller.spec.ts
@@ -4,6 +4,7 @@ import { CharactersService } from './characters.service';
 
 describe('CharactersController', () => {
   let controller: CharactersController;
+  let service: CharactersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,23 @@ describe('CharactersController', () => {
     }).compile();
 
     controller = module.get<CharactersController>(CharactersController);
+    service = module.get<CharactersService>(CharactersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return characters for a given page', async () => {
+    const characters = [];
+    const page = 2;
+     
+    //mokando o método findAll do serviço para retornar caracteres
+    jest.spyOn(service, 'findAll').mockResolvedValueOnce(characters);
+
+    // Afirmando que o controlador retorna caracteres para uma determinada página
+    expect(await controller.findAll(page)).toEqual(characters);
+    // Afirmando que o método findAll do serviço foi chamado com a página correta
+    expect(service.findAll).toHaveBeenCalledWith(page);    
+  })
 });
